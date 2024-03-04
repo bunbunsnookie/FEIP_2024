e21753ee1ac18a8819e37d119e70ecd3ed6f5112
bunbunsnookie <xbaka.so.baka@gmail.com>
2024-03-04 17:38:00 +1000

    stylization hello world + fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
+
+  <div class="background">
+
+    <div class="">
+      <span class="open_sans">Hello</span>
+      <span class="montserrat">, world!</span>
+    </div>
+    
+
   </div>
+  
 </template>
+
+
+<style>
+
+@import url(~/styles/base.css);
+
+</style>
